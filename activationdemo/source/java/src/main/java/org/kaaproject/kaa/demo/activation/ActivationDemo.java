8cf7a3569c4b686fa459d64318e001c58ea3c4be
Andrew Shvayka <ashvayka@cybervisiontech.com>
2016-02-19 13:32:44 +0200

    Endpoint Activation Demo fix

--- activationdemo/source/java/src/main/java/org/kaaproject/kaa/demo/activation/ActivationDemo.java
+++ activationdemo/source/java/src/main/java/org/kaaproject/kaa/demo/activation/ActivationDemo.java
@@ -43,7 +43,7 @@ import java.util.Map;
 public class ActivationDemo {
     private static final Logger LOG = LoggerFactory.getLogger(ActivationDemo.class);
 
-    private final static String APPLICATION_NAME = "Activation demo";
+    private final static String APPLICATION_NAME = "Endpoint activation demo";
 
     private static KaaClient kaaClient;
 

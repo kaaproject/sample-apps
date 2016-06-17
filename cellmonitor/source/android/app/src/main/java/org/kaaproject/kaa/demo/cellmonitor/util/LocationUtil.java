c142d0dc4a27b31cda71f891753ef972d109b894
maxml <maxml@inbox.ru>
2016-06-17 17:01:39 +0300

    APP-169 clean version

--- cellmonitor/source/android/app/src/main/java/org/kaaproject/kaa/demo/cellmonitor/util/LocationUtil.java
+++ cellmonitor/source/android/app/src/main/java/org/kaaproject/kaa/demo/cellmonitor/util/LocationUtil.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2014-2016 CyberVision, Inc.
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.kaaproject.kaa.demo.cellmonitor.util;
 
 import android.app.AlertDialog;
@@ -9,6 +25,10 @@ import android.provider.Settings;
 
 import org.kaaproject.kaa.demo.cellmonitor.R;
 
+/**
+ * Utility class for checking if user enable location on phone.
+ * Open dialog in other case.
+ */
 public class LocationUtil {
 
     public static boolean isLocationEnabled(Context context) {

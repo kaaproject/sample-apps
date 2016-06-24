dd9ea15bda3c4dbac414327e298ccefafdf4236d
maxml <maxml@inbox.ru>
2016-06-24 11:34:02 +0300

    APP-143 licence

--- notificationdemo/source/android/app/src/main/java/org/kaaproject/kaa/demo/notification/fragment/OnFragmentUpdateEvent.java
+++
@@ -1,8 +0,0 @@
-package org.kaaproject.kaa.demo.notification.fragment;
-
-public interface OnFragmentUpdateEvent {
-    /**
-     * Send event to fragments, they must update their views because of new information
-     */
-    void onRefresh();
-}

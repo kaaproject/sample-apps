a6bdbe5b95fbbcfaf3551d2a6c6d678fe9bf6edc
maxml <maxml@inbox.ru>
2016-07-07 18:36:18 +0300

    APP-92 credentials demo

---
+++ credentialsdemo/source/java/src/org/kaaproject/kaa/demo/credentials/CredentialsDemo.java
@@ -0,0 +1,115 @@
+/**
+ *  Copyright 2014-2016 CyberVision, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.kaaproject.kaa.demo.credentials;
+
+import org.kaaproject.kaa.demo.credentials.kaa.KaaAdminManager;
+import org.kaaproject.kaa.demo.credentials.kaa.KaaClientManager;
+import org.kaaproject.kaa.demo.credentials.utils.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A demo application that shows how to use the Kaa activation API.
+ */
+public class CredentialsDemo {
+    private static final Logger LOG = LoggerFactory.getLogger(CredentialsDemo.class);
+
+    public static void main(String[] args) throws InterruptedException {
+        LOG.info("Activation demo started");
+        if (args.length < 1) {
+            LOG.info("Invalid parameters");
+            LOG.info("Possible options:");
+            LOG.info(" java -jar CredentialsDemo.jar client");
+            LOG.info(" java -jar CredentialsDemo.jar admin");
+            return;
+
+        }
+        String mode = args[0];
+        switch (mode) {
+            case "admin":
+
+                inputAdminMenu();
+            case "client":
+                inputClientMenu();
+                break;
+            default:
+                LOG.info("Invalid parameters. Please specify 'client' or 'admin'");
+
+        }
+    }
+
+    private static void inputClientMenu() {
+        KaaClientManager manager = new KaaClientManager();
+        manager.start();
+
+
+        IOUtils.readSymbol();
+        /*
+         * Stop the Kaa client and connect it to the Kaa server.
+         */
+        manager.stop();
+    }
+
+    private static void inputAdminMenu() {
+        KaaAdminManager manager = new KaaAdminManager();
+
+        LOG.info("Choose action by entering corresponding number:");
+        while (true) {
+            LOG.info("\n1. Generate endpoint credentials.\n" +
+                    "2. Provision endpoint credentials.\n" +
+                    "3. Revoke endpoint credentials\n" +
+                    "4. Get credentials status\n" +
+                    "5. Exit");
+            switch (IOUtils.getUserInput()) {
+                case "1":
+                    LOG.info("You choose \"Generate endpoint credentials\".");
+                    manager.generateKeys();
+                    break;
+                case "2":
+                    LOG.info("You choose \"Provision endpoint credentials\".");
+                    manager.provisionKeys();
+                    break;
+                case "3":
+                    LOG.info("You choose \"Revoke endpoint credentials\".");
+                    manager.revokeCredentials();
+                    break;
+                case "4":
+                    LOG.info("You choose \"Get credentials status\".");
+                    LOG.info("Your credential sttaus - " + manager.getCredentialsStatus());
+                    break;
+                case "5":
+                    LOG.info("You choose \"Exit\". Have a good day!");
+                    System.exit(0);
+                    break;
+                default:
+                    LOG.info("You input not readable symbol. Please, input again.");
+            }
+        }
+
+    }
+}
+
+//                if (args.length < 2) {
+//                }
+//                if (args.length == 2) {
+//                    AdminClientManager.init(args[1], AdminClientManager.UserType.TENANT_ADMIN);
+//                } else if (args.length == 3) {
+//                    AdminClientManager.init(args[1], Integer.valueOf(args[2]), AdminClientManager.UserType.TENANT_ADMIN);
+//                } else {
+//                    LOG.info("ip/host is not specified or address is invalid");
+//                    return;
+//                }
---
+++ credentialsdemo/source/java/src/org/kaaproject/kaa/demo/credentials/kaa/KaaAdminManager.java
@@ -0,0 +1,227 @@
+package org.kaaproject.kaa.demo.credentials.kaa;
+
+import org.apache.http.auth.Credentials;
+import org.kaaproject.kaa.common.dto.ApplicationDto;
+import org.kaaproject.kaa.common.dto.admin.AuthResultDto;
+import org.kaaproject.kaa.common.dto.credentials.CredentialsDto;
+import org.kaaproject.kaa.common.dto.credentials.CredentialsStatus;
+import org.kaaproject.kaa.common.endpoint.security.KeyUtil;
+import org.kaaproject.kaa.demo.credentials.utils.CredentialsConstants;
+import org.kaaproject.kaa.demo.credentials.utils.IOUtils;
+import org.kaaproject.kaa.server.common.admin.AdminClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.List;
+
+/**
+ * Created by user083255 on 04.07.16.
+ */
+public class KaaAdminManager {
+
+    private static final Logger LOG = LoggerFactory.getLogger(KaaAdminManager.class);
+
+
+    private AdminClient adminClient;
+
+    public KaaAdminManager() {
+        this.adminClient = new AdminClient(CredentialsConstants.SANDBOX_IP_ADDRESS, CredentialsConstants.DEFAULT_KAA_PORT);
+    }
+
+    public void generateKeys() {
+        LOG.info("Going to generate credentials...");
+        try {
+            getOrCreateKeyPair();
+            LOG.info("Success generation!");
+        } catch (IOException e) {
+            LOG.error("Error in generateKeys", e);
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            LOG.error("Error in generateKeys", e);
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            LOG.error("Error in generateKeys", e);
+            e.printStackTrace();
+        }
+    }
+
+    public void provisionKeys() {
+        LOG.info("Going to provision credentials...");
+        try {
+            provideCredentials(CredentialsConstants.APPLICATION_NAME, getOrCreateKeyPair().getPublic().getEncoded());
+
+            LOG.info("Credentials is provisioning. Success!");
+        } catch (IOException e) {
+            LOG.error("Error in provisionKeys", e);
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            LOG.error("Error in provisionKeys", e);
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            LOG.error("Error in provisionKeys", e);
+            e.printStackTrace();
+        }
+    }
+
+    public void revokeCredentials() {
+        LOG.info("Enter ID of credentials that needs to be revoked:");
+        String credentialsId = IOUtils.getUserInput();
+        LOG.info("Going to revoke credentials");
+        revokeCredentials(CredentialsConstants.APPLICATION_NAME, credentialsId);
+    }
+
+    private void provideCredentials(String applicationName, byte[] publicKey) {
+        CredentialsDto credentialsDto = adminClient.provisionCredentials(getApplicationByName(applicationName)
+                .getApplicationToken(), publicKey);
+//        adminClient.provisionRegistration(getApplicationByName(applicationName).getApplicationToken(),
+//                credentialsDto.getId(), 1, "");
+        LOG.debug("APP TOKEN: {}", getApplicationByName(applicationName).getApplicationToken());
+        LOG.info("Credentials with ID={} are now in status: {}", credentialsDto.getId(), credentialsDto.getStatus());
+    }
+
+    private void revokeCredentials(String applicationName, String credentialsId) {
+        LOG.debug("APP TOKEN: {}", getApplicationByName(applicationName).getApplicationToken());
+        adminClient.revokeCredentials(getApplicationByName(applicationName).getApplicationToken(), credentialsId);
+        LOG.info("Credentials revoked. Success!");
+    }
+
+    public CredentialsStatus getCredentialsStatus() {
+        try {
+            byte[] publicKey = getOrCreateKeyPair().getPublic().getEncoded();
+            CredentialsDto credentialsDto = adminClient.provisionCredentials(
+                    getApplicationByName(CredentialsConstants.APPLICATION_NAME).getApplicationToken(), publicKey);
+
+            return credentialsDto.getStatus();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    /**
+     * Get application object by specified application name
+     *
+     * @param applicationName the application name
+     * @return the application object
+     */
+    private ApplicationDto getApplicationByName(String applicationName) {
+        checkAuthorizationAndLogin();
+
+        try {
+            List<ApplicationDto> applications = adminClient.getApplications();
+            for (ApplicationDto application : applications) {
+                if (application.getName().trim().equals(applicationName)) {
+                    return application;
+                }
+            }
+        } catch (Exception e) {
+            LOG.error("Exception has occurred: " + e.getMessage());
+        }
+        return null;
+    }
+
+    /**
+     * Checks authorization and log in
+     */
+    private void checkAuthorizationAndLogin() {
+        if (!checkAuth()) {
+            adminClient.login(CredentialsConstants.TENANT_ADMIN_USERNAME, CredentialsConstants.TENANT_ADMIN_PASSWORD);
+        }
+    }
+
+    /**
+     * Do authorization check
+     *
+     * @return true if user is authorized otherwise false
+     */
+    private boolean checkAuth() {
+        AuthResultDto.Result authResult = null;
+        try {
+            authResult = adminClient.checkAuth().getAuthResult();
+        } catch (Exception e) {
+            LOG.error("Exception has occurred: " + e.getMessage());
+        }
+        return authResult == AuthResultDto.Result.OK;
+    }
+
+
+    private KeyPair getOrCreateKeyPair() throws IOException, InvalidKeyException, NoSuchAlgorithmException {
+        if (!isExists()) {
+            return KeyUtil.generateKeyPair(CredentialsConstants.PRIVATE_KEY_LOCATION, CredentialsConstants.PUBLIC_KEY_LOCATION);
+        } else {
+            return getKeyPair();
+        }
+    }
+
+    private boolean isExists() {
+        return new File(CredentialsConstants.PRIVATE_KEY_LOCATION).exists() &&
+                new File(CredentialsConstants.PUBLIC_KEY_LOCATION).exists();
+    }
+
+    private KeyPair getKeyPair() throws IOException, InvalidKeyException {
+        InputStream publicKeyInput = new FileInputStream(new File(CredentialsConstants.PUBLIC_KEY_LOCATION));
+        InputStream privateKeyInput = new FileInputStream(new File(CredentialsConstants.PRIVATE_KEY_LOCATION));
+
+        PublicKey publicKey = KeyUtil.getPublic(publicKeyInput);
+        PrivateKey privateKey = KeyUtil.getPrivate(privateKeyInput);
+
+        return new KeyPair(publicKey, privateKey);
+    }
+}
+
+//    /**
+//     * Output all available endpoint profiles associated to activation
+//     * application
+//     *
+//     * @param endpointProfiles endpoint profiles associated to activation application
+//     */
+//    private static void printAllEndpointProfiles(Map<String, EndpointProfileDto> endpointProfiles) {
+//        LOG.info("Endpoint profiles: ");
+//        for (Map.Entry<String, EndpointProfileDto> entry : endpointProfiles.entrySet()) {
+//            EndpointProfileDto endpointProfile = entry.getValue();
+//            String endpointKeyHash = Base64.getEncoder().encodeToString(endpointProfile.getEndpointKeyHash());
+//            boolean isActive = DeviceState.parseJsonString(endpointProfile.getServerProfileBody());
+//            LOG.info("Profile id: {} endpointHash: {} device state: {}", entry.getKey(), endpointKeyHash, isActive ? "active" : "inactive");
+//        }
+//    }
+
+//    /**
+//     * Update the server profile object using REST API
+//     *
+//     * @param endpointProfile the endpointProfileDto object
+//     */
+//    public static void updateServerProfile(EndpointProfileDto endpointProfile) {
+//        LOG.info("Update server profile");
+//        String profileBody = endpointProfile.getServerProfileBody();
+//        boolean isActive = DeviceState.parseJsonString(profileBody);
+//        int version = endpointProfile.getServerProfileVersion();
+//        String endpointKeyHash = Base64.getEncoder().encodeToString(endpointProfile.getEndpointKeyHash());
+//        updateServerProfile(endpointKeyHash, version, !isActive);
+//    }
+
+//    /**
+//     * Update the server profile object using REST API
+//     *
+//     * @param endpointKeyHash the endpointKeyHash
+//     * @param profileVersion  the server profile version
+//     * @param newState        new device state
+//     */
+//    public static void updateServerProfile(String endpointKeyHash, int profileVersion, boolean newState) {
+//        LOG.info("Update server profile");
+//        AdminClientManager clientManager = AdminClientManager.instance();
+//        clientManager.updateServerProfile(endpointKeyHash, profileVersion, DeviceState.toJsonString(newState));
+//    }

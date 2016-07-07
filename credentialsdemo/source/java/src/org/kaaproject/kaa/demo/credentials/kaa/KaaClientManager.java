94046110f09b9cdc6f2ee4fb85e150a280d6938f
maxml <maxml@inbox.ru>
2016-07-07 18:36:18 +0300

    APP-92 credentials demo

---
+++ credentialsdemo/source/java/src/org/kaaproject/kaa/demo/credentials/kaa/KaaClientManager.java
@@ -0,0 +1,63 @@
+package org.kaaproject.kaa.demo.credentials.kaa;
+
+import org.kaaproject.kaa.client.DesktopKaaPlatformContext;
+import org.kaaproject.kaa.client.Kaa;
+import org.kaaproject.kaa.client.KaaClient;
+import org.kaaproject.kaa.client.SimpleKaaClientStateListener;
+import org.kaaproject.kaa.client.configuration.base.ConfigurationListener;
+import org.kaaproject.kaa.client.configuration.base.SimpleConfigurationStorage;
+import org.kaaproject.kaa.client.profile.ProfileContainer;
+import org.kaaproject.kaa.demo.activation.DeviceType;
+import org.kaaproject.kaa.schema.system.EmptyData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ */
+public class KaaClientManager {
+
+    private static final Logger LOG = LoggerFactory.getLogger(KaaClientManager.class);
+    private KaaClient kaaClient;
+
+    public void start() {
+        /*
+         * Create the Kaa desktop context for the application.
+         */
+        DesktopKaaPlatformContext desktopKaaPlatformContext = new DesktopKaaPlatformContext();
+
+        /*
+         * Create a Kaa client and add a listener which displays the Kaa client
+         * configuration as soon as the Kaa client is started.
+         */
+        kaaClient = Kaa.newClient(desktopKaaPlatformContext, new SimpleKaaClientStateListener() {
+            @Override
+            public void onStarted() {
+                super.onStarted();
+                LOG.info("Kaa client started");
+
+                LOG.info("Endpoint ID:" + kaaClient.getEndpointKeyHash());
+
+            }
+        });
+        kaaClient.setProfileContainer(new ProfileContainer() {
+            @Override
+            public EmptyData getProfile() {
+                return new EmptyData();
+            }
+        });
+        /*
+         * Persist configuration in a local storage to avoid downloading it each
+         * time the Kaa client is started.
+         */
+        kaaClient.setConfigurationStorage(new SimpleConfigurationStorage(desktopKaaPlatformContext, "saved_config.cfg"));
+
+        /*
+         * Start the Kaa client and connect it to the Kaa server.
+         */
+        kaaClient.start();
+    }
+
+    public void stop() {
+        kaaClient.stop();
+    }
+}
---
+++ credentialsdemo/source/java/src/org/kaaproject/kaa/demo/credentials/utils/CredentialsConstants.java
@@ -0,0 +1,19 @@
+package org.kaaproject.kaa.demo.credentials.utils;
+
+/**
+ * Created by user083255 on 04.07.16.
+ */
+public class CredentialsConstants {
+
+    public static final String SANDBOX_IP_ADDRESS = "10.2.3.113";
+    public static final int DEFAULT_KAA_PORT = 8080;
+
+    public static final String PRIVATE_KEY_LOCATION = "key.private";
+    public static final String PUBLIC_KEY_LOCATION = "key.public";
+
+    public static final String TENANT_ADMIN_USERNAME = "admin";
+    public static final String TENANT_ADMIN_PASSWORD = "admin123";
+
+    public final static String APPLICATION_NAME = "Endpoint activation demo";
+
+}

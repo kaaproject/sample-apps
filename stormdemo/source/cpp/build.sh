0239e7ae08f58097cc332627c147dfafb2174e3a
RostakaGmfun <rostawesomegd@gmail.com>
2016-08-08 16:36:06 +0300

    APP-146: Duplicate license in cassandra and storm demos

--- stormdemo/source/cpp/build.sh
+++ stormdemo/source/cpp/build.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #
 # Copyright 2014-2016 CyberVision, Inc.
 #
@@ -13,8 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-#!/bin/bash
 
 RUN_DIR=`pwd`
 

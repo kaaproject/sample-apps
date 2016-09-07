7b72852083193058bd6dae9e515d15f068f39f75
Rybachenko Andrey <arybachenko@cybervisiontech.com>
2016-09-07 15:52:46 +0300

    APP-93: Fix pom files. Refactor ProfilingDemoBuilder.java

--- profilingdemo/src/main/java/org/kaaproject/kaa/examples/profiling/ProfilingDemoBuilder.java
+++ profilingdemo/src/main/java/org/kaaproject/kaa/examples/profiling/ProfilingDemoBuilder.java
@@ -39,6 +39,11 @@ public class ProfilingDemoBuilder extends AbstractDemoBuilder {
 
     private static final Logger logger = LoggerFactory.getLogger(ProfilingDemoBuilder.class);
 
+    private AdminClient adminClient;
+    private ApplicationDto profilingApplication;
+    private ConfigurationSchemaDto configurationSchema;
+    private ServerProfileSchemaDto serverProfileSchema;
+
     public ProfilingDemoBuilder() {
         super("demo/profiling");
     }
@@ -48,12 +53,9 @@ public class ProfilingDemoBuilder extends AbstractDemoBuilder {
 
         logger.info("Loading 'Endpoint profiling demo application' data...");
 
-        loginTenantAdmin(client);
-        ApplicationDto profilingApplication = createApplicationDto(client, "Endpoint profiling demo", "Trustful");
+        initDemoBuilder(client);
 
-        loginTenantDeveloper(client);
-
-        EndpointGroupDto baseEndpointGroup = createBaseEndpointGroup(client, profilingApplication);
+        EndpointGroupDto baseEndpointGroup = createBaseEndpointGroup();
         if (baseEndpointGroup == null) {
             throw new RuntimeException("Can't get default endpoint group for profiling application!");
         }
@@ -61,75 +63,80 @@ public class ProfilingDemoBuilder extends AbstractDemoBuilder {
         // Cofigure sdk
         sdkProfileDto.setApplicationId(profilingApplication.getId());
         sdkProfileDto.setApplicationToken(profilingApplication.getApplicationToken());
-
         sdkProfileDto.setNotificationSchemaVersion(1);
         sdkProfileDto.setLogSchemaVersion(1);
 
         // Client profile schema
-        CTLSchemaDto endpointProfileCtlSchema = client.saveCTLSchemaWithAppToken(getResourceAsString("endpoint_profile_schema.avsc"),
+        CTLSchemaDto endpointProfileCtlSchema = adminClient.saveCTLSchemaWithAppToken(getResourceAsString("endpoint_profile_schema.avsc"),
                 profilingApplication.getTenantId(), profilingApplication.getApplicationToken());
 
-        createClientProfile(client, profilingApplication, endpointProfileCtlSchema,
-                "Endpoint profiling endpoint profile schema", "Endpoint profile schema describing profiling application profile");
-
-        // Configuration schema
-        CTLSchemaDto configurationCTLSchema = saveCTLSchemaWithAppToken(client, "configuration-schema.avsc", profilingApplication);
-        ConfigurationSchemaDto configurationSchema = createConfigurationSchema(client, profilingApplication, configurationCTLSchema,
-                "Endpoint profiling configuration schema", "Configuration schema describing profiling application profile");
+        createClientProfile(endpointProfileCtlSchema, "Endpoint profiling endpoint profile schema",
+                "Endpoint profile schema describing profiling application profile");
 
         // Configuration
-        editActivateConfiguration(client, profilingApplication, configurationSchema, baseEndpointGroup,
-                "Base endpoint profiling configuration", "configuration_devices.json", UpdateStatus.INACTIVE);
+        editActivateConfiguration(baseEndpointGroup, "Base endpoint profiling configuration", "configuration_devices.json", UpdateStatus.INACTIVE);
         sdkProfileDto.setConfigurationSchemaVersion(configurationSchema.getVersion());
 
-        // Profile groups
-        ServerProfileSchemaDto serverProfileSchema = createServerSchema(client, profilingApplication,
-                "server_profile_schema.avsc", "Endpoint server-side profile schema", "Auto-generated description");
-
         // first
-        EndpointGroupDto endpointGroup = createProfileGroup(client, profilingApplication, "Audio and vibration support", "Auto-generated", 2);
-
-        createProfileGroupConfiguration(client, profilingApplication, endpointGroup, configurationSchema,
-                "Auto-generated configuration schema", "group_audio_vibration_conf.json", UpdateStatus.INACTIVE);
-        createProfileGroupFilter(client, profilingApplication, endpointGroup, serverProfileSchema,
-                "group_audio_vibration_filter.json", "Auto-generated description", UpdateStatus.INACTIVE);
+        EndpointGroupDto endpointGroup = createProfileGroup("Audio and vibration support", "Auto-generated", 2);
+        createProfileGroupConfiguration(endpointGroup, "Auto-generated configuration schema", "group_audio_vibration_conf.json", UpdateStatus.INACTIVE);
+        createProfileGroupFilter(endpointGroup, "group_audio_vibration_filter.json", "Auto-generated description", UpdateStatus.INACTIVE);
 
         //second
-        endpointGroup = createProfileGroup(client, profilingApplication, "Only vibration support", "Auto-generated", 5);
-
-        createProfileGroupConfiguration(client, profilingApplication, endpointGroup, configurationSchema,
-                "Auto-generated configuration schema", "group_only_vibration_conf.json", UpdateStatus.INACTIVE);
-        createProfileGroupFilter(client, profilingApplication, endpointGroup, serverProfileSchema,
-                "group_only_vibration_filter.json", "Auto-generated description", UpdateStatus.INACTIVE);
+        endpointGroup = createProfileGroup("Only vibration support", "Auto-generated", 5);
+        createProfileGroupConfiguration(endpointGroup, "Auto-generated configuration schema", "group_only_vibration_conf.json", UpdateStatus.INACTIVE);
+        createProfileGroupFilter(endpointGroup, "group_only_vibration_filter.json", "Auto-generated description", UpdateStatus.INACTIVE);
 
         //third
-        endpointGroup = createProfileGroup(client, profilingApplication, "All services support", "Auto-generated", 7);
-        createProfileGroupConfiguration(client, profilingApplication, endpointGroup, configurationSchema,
-                "Auto-generated configuration schema", "group_all_conf.json", UpdateStatus.INACTIVE);
-        createProfileGroupFilter(client, profilingApplication, endpointGroup, serverProfileSchema,
-                "group_all_filter.json", "Auto-generated description", UpdateStatus.INACTIVE);
+        endpointGroup = createProfileGroup("All services support", "Auto-generated", 7);
+        createProfileGroupConfiguration( endpointGroup, "Auto-generated configuration schema", "group_all_conf.json", UpdateStatus.INACTIVE);
+        createProfileGroupFilter(endpointGroup, "group_all_filter.json", "Auto-generated description", UpdateStatus.INACTIVE);
 
         //fourth
-        endpointGroup = createProfileGroup(client, profilingApplication, "Disable vibration support for those who have audio and video", "Auto-generated", 10);
-        createProfileGroupConfiguration(client, profilingApplication, endpointGroup, configurationSchema,
-                "Auto-generated configuration schema", "group_disable_vibro_conf.json", UpdateStatus.INACTIVE);
-        createProfileGroupFilter(client, profilingApplication, endpointGroup, serverProfileSchema,
-                "group_disable_vibro_filter.json", "Auto-generated description", UpdateStatus.INACTIVE);
+        endpointGroup = createProfileGroup("Disable vibration support for those who have audio and video", "Auto-generated", 10);
+        createProfileGroupConfiguration(endpointGroup, "Auto-generated configuration schema", "group_disable_vibro_conf.json", UpdateStatus.INACTIVE);
+        createProfileGroupFilter(endpointGroup, "group_disable_vibro_filter.json", "Auto-generated description", UpdateStatus.INACTIVE);
+
+        clearDemoBuilder();
 
         logger.info("Finished loading 'Endpoint profiling demo application' data...");
     }
 
-    private EndpointGroupDto createBaseEndpointGroup(AdminClient client, ApplicationDto profilingApplication) throws Exception {
+
+
+    private void initDemoBuilder(AdminClient client) throws Exception {
+        adminClient = client;
+
+        loginTenantAdmin(adminClient);
+        profilingApplication = createApplicationDto("Endpoint profiling demo", "Trustful");
+
+        loginTenantDeveloper(adminClient);
+
+        // Configuration schema
+        CTLSchemaDto configurationCTLSchema = saveCTLSchemaWithAppToken(adminClient, "configuration-schema.avsc", profilingApplication);
+        configurationSchema = createConfigurationSchema(configurationCTLSchema, "Endpoint profiling configuration schema",
+                                                        "Configuration schema describing profiling application profile");
+
+        serverProfileSchema = createServerSchema("server_profile_schema.avsc", "Endpoint server-side profile schema", "Auto-generated description");
+    }
+
+    private void clearDemoBuilder() {
+        adminClient = null;
+        profilingApplication = null;
+        configurationSchema = null;
+        serverProfileSchema = null;
+    }
+
+    private EndpointGroupDto createBaseEndpointGroup() throws Exception {
         EndpointGroupDto baseEndpointGroup = null;
-        List<EndpointGroupDto> endpointGroups = client.getEndpointGroupsByAppToken(profilingApplication.getApplicationToken());
+        List<EndpointGroupDto> endpointGroups = adminClient.getEndpointGroupsByAppToken(profilingApplication.getApplicationToken());
         if (endpointGroups.size() == 1 && endpointGroups.get(0).getWeight() == 0) {
             baseEndpointGroup = endpointGroups.get(0);
         }
         return baseEndpointGroup;
     }
 
-    private void editActivateConfiguration(AdminClient client, ApplicationDto profilingApplication, ConfigurationSchemaDto configurationSchema,
-                                           EndpointGroupDto baseEndpointGroup,
+    private void editActivateConfiguration(EndpointGroupDto baseEndpointGroup,
                                            String confDesc, String confFile, UpdateStatus status) throws Exception {
 
         ConfigurationDto baseConfiguration = new ConfigurationDto();
@@ -143,16 +150,15 @@ public class ProfilingDemoBuilder extends AbstractDemoBuilder {
         baseConfiguration.setStatus(status);
 
         logger.info("Editing the configuration...");
-        baseConfiguration = client.editConfiguration(baseConfiguration);
+        baseConfiguration = adminClient.editConfiguration(baseConfiguration);
         logger.info("Configuration was successfully edited");
 
         logger.info("Activating the configuration");
-        client.activateConfiguration(baseConfiguration.getId());
+        adminClient.activateConfiguration(baseConfiguration.getId());
         logger.info("Configuration was activated");
     }
 
-    private ConfigurationSchemaDto createConfigurationSchema(AdminClient client, ApplicationDto profilingApplication, CTLSchemaDto configurationCTLSchema,
-                                                             String confName, String confDesc) throws Exception {
+    private ConfigurationSchemaDto createConfigurationSchema(CTLSchemaDto configurationCTLSchema, String confName, String confDesc) throws Exception {
 
         ConfigurationSchemaDto configurationSchema = new ConfigurationSchemaDto();
         configurationSchema.setApplicationId(profilingApplication.getId());
@@ -160,7 +166,7 @@ public class ProfilingDemoBuilder extends AbstractDemoBuilder {
         configurationSchema.setDescription(confDesc);
         configurationSchema.setCtlSchemaId(configurationCTLSchema.getId());
 
-        configurationSchema = client.saveConfigurationSchema(configurationSchema);
+        configurationSchema = adminClient.saveConfigurationSchema(configurationSchema);
 
         logger.info("Configuration schema version: {}", configurationSchema.getVersion());
 
@@ -170,28 +176,26 @@ public class ProfilingDemoBuilder extends AbstractDemoBuilder {
         return configurationSchema;
     }
 
-    private void createClientProfile(AdminClient client, ApplicationDto profilingApplication, CTLSchemaDto endpointProfileCtlSchema,
-                                     String profileName, String profileDescription) throws Exception {
+    private void createClientProfile(CTLSchemaDto endpointProfileCtlSchema, String profileName, String profileDescription) throws Exception {
         EndpointProfileSchemaDto endpointProfileSchema = new EndpointProfileSchemaDto();
         endpointProfileSchema.setApplicationId(profilingApplication.getId());
         endpointProfileSchema.setName(profileName);
         endpointProfileSchema.setDescription(profileDescription);
         endpointProfileSchema.setCtlSchemaId(endpointProfileCtlSchema.getId());
-        endpointProfileSchema = client.saveProfileSchema(endpointProfileSchema);
+        endpointProfileSchema = adminClient.saveProfileSchema(endpointProfileSchema);
         sdkProfileDto.setProfileSchemaVersion(endpointProfileSchema.getVersion());
     }
 
-    private ApplicationDto createApplicationDto(AdminClient client, String appName, String credentialsService) throws Exception {
+    private ApplicationDto createApplicationDto(String appName, String credentialsService) throws Exception {
         ApplicationDto profilingApplication = new ApplicationDto();
         profilingApplication.setName(appName);
         profilingApplication.setCredentialsServiceName(credentialsService);
 
-        profilingApplication = client.editApplication(profilingApplication);
+        profilingApplication = adminClient.editApplication(profilingApplication);
         return profilingApplication;
     }
 
-    private EndpointGroupDto createProfileGroup(AdminClient client, ApplicationDto profilingApplication, String groupName, String description, int weight)
-            throws Exception {
+    private EndpointGroupDto createProfileGroup(String groupName, String description, int weight) throws Exception {
 
         EndpointGroupDto endpointGroup = new EndpointGroupDto();
         endpointGroup.setApplicationId(profilingApplication.getId());
@@ -199,13 +203,12 @@ public class ProfilingDemoBuilder extends AbstractDemoBuilder {
         endpointGroup.setDescription(description);
         endpointGroup.setWeight(weight);
 
-        endpointGroup = client.editEndpointGroup(endpointGroup);
+        endpointGroup = adminClient.editEndpointGroup(endpointGroup);
         return endpointGroup;
     }
 
-    private void createProfileGroupFilter(AdminClient client, ApplicationDto profilingApplication, EndpointGroupDto endpointGroup,
-                                          ServerProfileSchemaDto serverProfileSchema,
-                                          String filterNameFile, String filterDesc, UpdateStatus filterStatus) throws Exception {
+    private void createProfileGroupFilter(EndpointGroupDto endpointGroup, String filterNameFile, String filterDesc, UpdateStatus filterStatus)
+                throws Exception {
 
         ProfileFilterDto INACTIVEProfileFilter = new ProfileFilterDto();
         INACTIVEProfileFilter.setApplicationId(profilingApplication.getId());
@@ -216,12 +219,11 @@ public class ProfilingDemoBuilder extends AbstractDemoBuilder {
         INACTIVEProfileFilter.setBody(FileUtils.readResource(getResourcePath(filterNameFile)));
         INACTIVEProfileFilter.setStatus(filterStatus);
 
-        INACTIVEProfileFilter = client.editProfileFilter(INACTIVEProfileFilter);
-        client.activateProfileFilter(INACTIVEProfileFilter.getId());
+        INACTIVEProfileFilter = adminClient.editProfileFilter(INACTIVEProfileFilter);
+        adminClient.activateProfileFilter(INACTIVEProfileFilter.getId());
     }
 
-    private void createProfileGroupConfiguration(AdminClient client, ApplicationDto profilingApplication, EndpointGroupDto endpointGroup,
-                                                 ConfigurationSchemaDto configurationSchema,
+    private void createProfileGroupConfiguration(EndpointGroupDto endpointGroup,
                                                  String confDesc, String confFilename, UpdateStatus status) throws Exception {
 
         ConfigurationDto profileGroupConfiguration = new ConfigurationDto();
@@ -233,19 +235,17 @@ public class ProfilingDemoBuilder extends AbstractDemoBuilder {
         profileGroupConfiguration.setBody(FileUtils.readResource(getResourcePath(confFilename)));
         profileGroupConfiguration.setStatus(status);
 
-        profileGroupConfiguration = client.editConfiguration(profileGroupConfiguration);
-        client.activateConfiguration(profileGroupConfiguration.getId());
+        profileGroupConfiguration = adminClient.editConfiguration(profileGroupConfiguration);
+        adminClient.activateConfiguration(profileGroupConfiguration.getId());
     }
 
-    private ServerProfileSchemaDto createServerSchema(AdminClient client, ApplicationDto profilingApplication,
-                                                      String fileName, String serverName, String serverDesc) throws Exception {
+    private ServerProfileSchemaDto createServerSchema(String fileName, String serverName, String serverDesc) throws Exception {
 
-        CTLSchemaDto serverProfileCtlSchema = saveCTLSchemaWithAppToken(client, fileName, profilingApplication);
-        return getServerProfileSchemaDto(client, profilingApplication, serverProfileCtlSchema, serverName, serverDesc);
+        CTLSchemaDto serverProfileCtlSchema = saveCTLSchemaWithAppToken(adminClient, fileName, profilingApplication);
+        return getServerProfileSchemaDto(serverProfileCtlSchema, serverName, serverDesc);
     }
 
-    private ServerProfileSchemaDto getServerProfileSchemaDto(AdminClient client, ApplicationDto profilingApplication, CTLSchemaDto serverProfileCtlSchema,
-                                                             String serverName, String serverDesc) throws Exception {
+    private ServerProfileSchemaDto getServerProfileSchemaDto(CTLSchemaDto serverProfileCtlSchema, String serverName, String serverDesc) throws Exception {
 
         ServerProfileSchemaDto serverProfileSchema = new ServerProfileSchemaDto();
         serverProfileSchema.setApplicationId(profilingApplication.getId());
@@ -253,8 +253,9 @@ public class ProfilingDemoBuilder extends AbstractDemoBuilder {
         serverProfileSchema.setDescription(serverDesc);
         serverProfileSchema.setCtlSchemaId(serverProfileCtlSchema.getId());
 
-        serverProfileSchema = client.saveServerProfileSchema(serverProfileSchema);
+        serverProfileSchema = adminClient.saveServerProfileSchema(serverProfileSchema);
 
         return serverProfileSchema;
     }
+
 }

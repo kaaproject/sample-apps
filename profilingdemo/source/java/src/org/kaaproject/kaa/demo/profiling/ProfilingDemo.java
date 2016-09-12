ef2666001a4a7e5ca3462c4b57bb47a886b01968
Rybachenko Andrey <arybachenko@cybervisiontech.com>
2016-09-12 13:38:07 +0300

    APP-93: Refactor and fix Profiling demo App. Fix Profiling demo Builder

--- profilingdemo/source/java/src/org/kaaproject/kaa/demo/profiling/ProfilingDemo.java
+++ profilingdemo/source/java/src/org/kaaproject/kaa/demo/profiling/ProfilingDemo.java
@@ -30,6 +30,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
@@ -39,16 +40,14 @@ import java.util.concurrent.TimeUnit;
 public class ProfilingDemo {
 
     private static final Logger LOG = LoggerFactory.getLogger(ProfilingDemo.class);
-    /**
-     * In this dir all Kaa clients creates it's keys for starting
-     */
-    public static final String KAA_CLIENT_WORKING_DIR = "./res/kaaClientWorkingDir";
 
-    private static ArrayList<KaaClient> kaaClients = new ArrayList<>();
-    /**
-     * Collection of client client-side profiles, that change on client side
-     */
-    private static ArrayList<PagerClientProfile> profiles = new ArrayList<>();
+    // Path for directory to store endpoints working subdirectories
+    private static final String KAA_CLIENT_WORKING_DIR = "./res/kaaClientWorkingDir";
+
+    private static List<KaaClient> kaaClients = new ArrayList<>();
+
+    // Client-side profiles list, for creating endpoints
+    private static List<PagerClientProfile> profiles = new ArrayList<>();
 
     static {
         profiles.add(new PagerClientProfile(false, false, true));
@@ -59,63 +58,58 @@ public class ProfilingDemo {
     public static void main(String[] args) throws IOException {
         LOG.info("Profiling demo started");
 
-        for (int i = 0; i < profiles.size(); i++) {
-            try {
-                startKaaClient(i);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        // Creating endpoints using KaaClient class
+        kaaClients = createKaaClients(profiles);
+
+        // Start
+        for (KaaClient client: kaaClients) {
+            client.start();
         }
 
-        try {
-            TimeUnit.SECONDS.sleep(5);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        // Waiting for clients to start and display theirs profiles/configuration
+        sleepForSeconds(5);
 
-        LOG.info("--= Press any key to exit =--");
-        try {
-            System.in.read();
-        } catch (IOException e) {
-            LOG.error("IOException was caught - ", e);
-        }
+        waitForAnyKeyPress();
 
-        /*
-         * Stop the Kaa client and release all the resources which were in use.
-         */
-        try {
-            stopKaaClients();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // Stop the Kaa clients and release all the resources
+        stopKaaClients(kaaClients);
 
         LOG.info("Profiling demo stopped");
     }
 
-    public static void startKaaClient(final int index) throws IOException {
 
+    private static List<KaaClient> createKaaClients(List<PagerClientProfile> profiles) throws IOException {
+        List<KaaClient> clients = new ArrayList<>();
+        for (int i = 0; i < profiles.size(); i++) {
+            try {
+                KaaClient client = createKaaClient(i);
+                clients.add(client);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return clients;
+    }
+
+    private static KaaClient createKaaClient(final int index) throws IOException {
+
+        // Setup separate working folder for endpoint
         KaaClientProperties kaaClientProperties = new KaaClientProperties();
         kaaClientProperties.setWorkingDirectory(KAA_CLIENT_WORKING_DIR + index);
 
-        /*
-         * Create the Kaa desktop context for the application.
-         */
+        // Create the Kaa desktop context for the application.
         DesktopKaaPlatformContext desktopKaaPlatformContext = new DesktopKaaPlatformContext(kaaClientProperties);
 
-        /*
-         * Create a Kaa client and add a listener which displays the Kaa client profiling
-         * as soon as the Kaa client is started.
-         */
+        // Create a Kaa client with listener which displays endpoint's profile/configuration data after client start
         final KaaClient kaaClient = Kaa.newClient(desktopKaaPlatformContext, new SimpleKaaClientStateListener() {
             @Override
             public void onStarted() {
                 super.onStarted();
-
                 displayConfiguration(index);
             }
         }, true);
-        kaaClients.add(kaaClient);
 
+        // Setup endpoint profile
         kaaClient.setProfileContainer(new ProfileContainer() {
             @Override
             public PagerClientProfile getProfile() {
@@ -123,61 +117,71 @@ public class ProfilingDemo {
             }
         });
 
-        /*
-         * Persist profiling in a local storage to avoid downloading it each time the Kaa client is started.
-         */
-        kaaClient.setConfigurationStorage(new SimpleConfigurationStorage(desktopKaaPlatformContext,
-                KAA_CLIENT_WORKING_DIR + index + "/saved_config.cfg"));
+        // Persist configuration in a local storage to avoid downloading it each time the Kaa client is started.
+        kaaClient.setConfigurationStorage(new SimpleConfigurationStorage(desktopKaaPlatformContext, KAA_CLIENT_WORKING_DIR + index + "/saved_config.cfg"));
 
-        /*
-         * Add a listener which displays the Kaa client profiling each time it is updated.
-         */
+        //Add a listener to display endpoint's profile/configuration data each time configuration is updated.
         kaaClient.addConfigurationListener(new ConfigurationListener() {
             @Override
             public void onConfigurationUpdate(PagerConfiguration configuration) {
+                LOG.info("Endpoint #{} configuration was updated.", index);
                 displayConfiguration(index);
             }
         });
 
-        /*
-         * Start the Kaa client and connect it to the Kaa server.
-         */
-        kaaClient.start();
+        return kaaClient;
     }
 
-    public static void stopKaaClients() throws IOException {
-        LOG.info("Stopping clients...");
-        for (KaaClient k : kaaClients) {
-            if (k != null)
-                k.stop();
+    private static void sleepForSeconds(int seconds) {
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
     public synchronized static void displayConfiguration(int index) {
         if (index >= kaaClients.size()) {
-            LOG.error("Index more than KaaClient's list size!");
+            LOG.error("Endpoint #{} not found: Kaa client #{} not found.", index, index);
             return;
         }
 
         KaaClient kaaClient = kaaClients.get(index);
         if (kaaClient != null) {
-            PagerClientProfile data = profiles.get(index);
-            LOG.info("{}-th profiling body (have audio-, video-, vibration-support):" + index);
-            LOG.info("{} - {} - {}", data.getAudioSupport(), data.getVideoSupport(), data.getVibroSupport());
 
-            PagerConfiguration configuration = kaaClient.getConfiguration();
+            LOG.info("Endpoint #{} data:", index);
+            LOG.info("KeyHash - {}", kaaClient.getEndpointKeyHash());
 
-            boolean isAudioSupport = configuration.getAudioSubscriptionActive();
-            boolean isVideoSupport = configuration.getVideoSubscriptionActive();
-            boolean isVibroSupport = configuration.getVibroSubscriptionActive();
+            PagerClientProfile profile = profiles.get(index);
+            LOG.info("Client-side endpoint profile (audio/video/vibro support): {} - {} - {}",
+                            profile.getAudioSupport(), profile.getVideoSupport(), profile.getVibroSupport());
 
-            LOG.info("KeyHash - " + kaaClient.getEndpointKeyHash());
-
-            LOG.info("{}-th profiling body (have audio-, video-, vibration-support):" + index);
-            LOG.info("{} - {} - {}", isAudioSupport, isVideoSupport, isVibroSupport);
+            PagerConfiguration conf = kaaClient.getConfiguration();
+            LOG.info("Configuration (is audio/video/vibro subscription active): {} - {} - {}",
+                            conf.getAudioSubscriptionActive(), conf.getVideoSubscriptionActive(), conf.getVibroSubscriptionActive());
 
         } else {
             LOG.info("Kaa Client is null!");
+        }
+    }
+
+    private static void waitForAnyKeyPress() {
+        LOG.info("--= Press any key to exit =--");
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            LOG.error("IOException was caught - ", e);
+        }
+    }
+
+    private static void stopKaaClients(List<KaaClient> clients) {
+        LOG.info("Stopping clients...");
+        for (KaaClient client : clients) {
+            try {
+                client.stop();
+            } catch (Exception e) { // catch NullPointerException as well as IOException
+                e.printStackTrace();
+            }
         }
     }
 }

920e8a5c881f52930d598e6b3e927e61ce5c29d8
Aleksey Gutyro <agutyro@cybervisiontech.com>
2016-09-19 17:56:33 +0300

    Add CredentialsDemoFailoverStrategy.

---
+++ credentialsdemo/source/objc/CredentialsDemo/CredentialsDemoFailoverStrategy.h
@@ -0,0 +1,15 @@
+//
+//  CredentialsDemoFailoverStrategy.h
+//  CredentialsDemo
+//
+//  Created by Aleksey Gutyro on 19.09.16.
+//  Copyright © 2016 CYBERVISION INC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@import Kaa;
+
+@interface CredentialsDemoFailoverStrategy : DefaultFailoverStrategy
+
+@end
---
+++ credentialsdemo/source/objc/CredentialsDemo/CredentialsDemoFailoverStrategy.m
@@ -0,0 +1,13 @@
+//
+//  CredentialsDemoFailoverStrategy.m
+//  CredentialsDemo
+//
+//  Created by Aleksey Gutyro on 19.09.16.
+//  Copyright © 2016 CYBERVISION INC. All rights reserved.
+//
+
+#import "CredentialsDemoFailoverStrategy.h"
+
+@implementation CredentialsDemoFailoverStrategy
+
+@end

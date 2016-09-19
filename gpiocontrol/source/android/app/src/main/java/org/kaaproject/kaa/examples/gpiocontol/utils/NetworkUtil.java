494920722ed2e5b6238417c7ecae9b6c8df19be7
The Todor <todor.ilya@gmail.com>
2016-09-19 16:43:11 +0300

    APP-172: GPIO was moved to gradle

---
+++ gpiocontrol/source/android/app/src/main/java/org/kaaproject/kaa/examples/gpiocontol/utils/NetworkUtil.java
@@ -0,0 +1,40 @@
+package org.kaaproject.kaa.examples.gpiocontol.utils;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
+
+import org.kaaproject.kaa.examples.gpiocontrol.R;
+
+public class NetworkUtil {
+
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        return (networkInfo != null && networkInfo.isConnected());
+    }
+
+    public static AlertDialog showNetworkDialog(final Context context) {
+        return new AlertDialog.Builder(context)
+                .setTitle(R.string.no_internet_connection)
+                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        context.startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+                    }
+                })
+                .show();
+    }
+
+}

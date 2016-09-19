240e260c3b8cdfe2c7c955408fa7d2ea49d080ca
The Todor <todor.ilya@gmail.com>
2016-09-19 16:43:11 +0300

    APP-172: GPIO was moved to gradle

---
+++ gpiocontrol/source/android/app/src/main/java/org/kaaproject/kaa/examples/gpiocontol/DevicesListActivity.java
@@ -0,0 +1,225 @@
+/**
+ *  Copyright 2014-2016 CyberVision, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.kaaproject.kaa.examples.gpiocontol;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import org.kaaproject.kaa.client.KaaClient;
+import org.kaaproject.kaa.client.connectivity.AndroidConnectivityChecker;
+import org.kaaproject.kaa.client.event.EndpointAccessToken;
+import org.kaaproject.kaa.client.event.EndpointKeyHash;
+import org.kaaproject.kaa.client.event.registration.OnAttachEndpointOperationCallback;
+import org.kaaproject.kaa.common.endpoint.gen.SyncResponseResultType;
+import org.kaaproject.kaa.examples.gpiocontol.adapters.DevicesAdapter;
+import org.kaaproject.kaa.examples.gpiocontol.model.Device;
+import org.kaaproject.kaa.examples.gpiocontol.utils.KaaProvider;
+import org.kaaproject.kaa.examples.gpiocontol.utils.NetworkUtil;
+import org.kaaproject.kaa.examples.gpiocontol.utils.PreferencesManager;
+import org.kaaproject.kaa.examples.gpiocontrol.DeviceInfoRequest;
+import org.kaaproject.kaa.examples.gpiocontrol.DeviceInfoResponse;
+import org.kaaproject.kaa.examples.gpiocontrol.R;
+import org.kaaproject.kaa.examples.gpiocontrol.RemoteControlECF;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DevicesListActivity extends AppCompatActivity {
+
+    private final String TAG = DevicesListActivity.class.getSimpleName();
+
+    private AndroidConnectivityChecker connectivityChecker;
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+
+    private ProgressBar progressBar;
+
+    private KaaClient kaaClient;
+    private final Context context = this;
+
+    private String endpointId;
+    private List<Device> devices;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_devices_list);
+
+        initViews();
+
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        devices = new ArrayList<>();
+        mAdapter = new DevicesAdapter(devices);
+        mRecyclerView.setAdapter(mAdapter);
+
+        connectivityChecker = new AndroidConnectivityChecker(this);
+        startKaa();
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        refreshActivity();
+    }
+
+    private void refreshActivity() {
+        if (isFirstLaunch()) {
+            devices.clear();
+            mAdapter.notifyDataSetChanged();
+            kaaClient.getEventFamilyFactory().getRemoteControlECF().sendEventToAll(new DeviceInfoRequest());
+        }
+    }
+
+    private void addDevice(Device device) {
+        if (devices.contains(device)) {
+            int index = devices.indexOf(device);
+            devices.set(index, device);
+            mAdapter.notifyItemChanged(index);
+        } else {
+            devices.add(device);
+            mAdapter.notifyItemInserted(devices.size() - 1);
+        }
+        findViewById(R.id.noEndpointsText).setVisibility(View.INVISIBLE);
+    }
+
+    private void startKaa() {
+        progressBar.setVisibility(View.VISIBLE);
+        if (!NetworkUtil.isNetworkAvailable(this)) {
+            NetworkUtil.showNetworkDialog(this);
+        }else {
+            kaaClient = KaaProvider.getClient(this);
+            kaaClient.start();
+            if (isFirstLaunch()) {
+                PreferencesManager.setUserExternalId(this, "2");
+                Log.d(TAG, "Attaching user...");
+                KaaProvider.attachUser(context);
+            }
+            setUpEndpointListener();
+        }
+        progressBar.setVisibility(View.INVISIBLE);
+    }
+
+    private void setUpEndpointListener(){
+        if (!NetworkUtil.isNetworkAvailable(this)) {
+            NetworkUtil.showNetworkDialog(this);
+            return;
+        }
+        if (kaaClient == null) {
+            startKaa();
+        }
+
+        KaaProvider.setUpEventListener(this, new RemoteControlECF.Listener() {
+            @Override
+            public void onEvent(DeviceInfoResponse deviceInfoResponse, String endpointId) {
+                Log.d(TAG, "Got DeviceInfoResponse");
+                Device device = new Device(
+                        deviceInfoResponse.getModel(),
+                        deviceInfoResponse.getDeviceName(),
+                        deviceInfoResponse.getGpioStatus(),
+                        endpointId);
+                addDevice(device);
+            }
+        });
+    }
+
+    private void showEndpointDialog() {
+        LayoutInflater layoutInflater = LayoutInflater.from(DevicesListActivity.this);
+        View promptView = layoutInflater.inflate(R.layout.dialog_endpoint_id, null);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(DevicesListActivity.this);
+        alertDialogBuilder.setView(promptView);
+
+        final EditText editText = (EditText) promptView.findViewById(R.id.edittext);
+        alertDialogBuilder.setCancelable(false)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        if (kaaClient == null) {
+                            startKaa();
+                        }
+
+                        endpointId = editText.getText().toString();
+                        progressBar.setVisibility(View.VISIBLE);
+
+                        if (endpointId == null || endpointId.isEmpty()) {
+                            Snackbar.make(mRecyclerView, "Endpoint ID can't be empty", Snackbar.LENGTH_SHORT).show();
+                            return;
+                        }
+                        kaaClient.attachEndpoint(new EndpointAccessToken(endpointId), new OnAttachEndpointOperationCallback() {
+                            @Override
+                            public void onAttach(SyncResponseResultType syncResponseResultType, EndpointKeyHash endpointKeyHash) {
+                                Log.d(TAG, "attachEndpoint result: " + syncResponseResultType.toString());
+                            }
+                        });
+                        KaaProvider.sendDeviceInfoRequestToAll(context);
+                        progressBar.setVisibility(View.INVISIBLE);
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
+    }
+
+    private boolean isFirstLaunch(){
+        return PreferencesManager.getUserExternalId(this).isEmpty();
+    }
+
+    private void initViews() {
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        ((TextView) findViewById(R.id.appName)).setText(getText(R.string.app_name));
+        findViewById(R.id.reloadButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                refreshActivity();
+            }
+        });
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.attachToRecyclerView(mRecyclerView);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showEndpointDialog();
+            }
+        });
+    }
+}
+
---
+++ gpiocontrol/source/android/app/src/main/java/org/kaaproject/kaa/examples/gpiocontol/adapters/GPIOAdapter.java
@@ -0,0 +1,114 @@
+/**
+ *  Copyright 2014-2016 CyberVision, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.kaaproject.kaa.examples.gpiocontol.adapters;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SwitchCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import org.kaaproject.kaa.client.KaaClient;
+import org.kaaproject.kaa.client.event.EventFamilyFactory;
+import org.kaaproject.kaa.examples.gpiocontol.model.Device;
+import org.kaaproject.kaa.examples.gpiocontol.utils.KaaProvider;
+import org.kaaproject.kaa.examples.gpiocontol.utils.NetworkUtil;
+import org.kaaproject.kaa.examples.gpiocontrol.GpioStatus;
+import org.kaaproject.kaa.examples.gpiocontrol.GpioToggleRequest;
+import org.kaaproject.kaa.examples.gpiocontrol.R;
+import org.kaaproject.kaa.examples.gpiocontrol.RemoteControlECF;
+
+import java.util.List;
+
+public class GPIOAdapter extends RecyclerView.Adapter<GPIOAdapter.ViewHolder> {
+
+    private List<GpioStatus> gpioStatusList;
+    private Device device;
+
+    private Context context;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public boolean state;
+        public CardView cardView;
+        public TextView gpioId;
+        public SwitchCompat switcher;
+
+        public ViewHolder(CardView holderView) {
+            super(holderView);
+            cardView = holderView;
+            gpioId = (TextView)cardView.findViewById(R.id.gpioId);
+            switcher = (SwitchCompat)cardView.findViewById(R.id.switcher);
+            state = false;
+        }
+    }
+
+    public GPIOAdapter(Context context, Device device) {
+        this.device = device;
+        this.context = context;
+        gpioStatusList = device.getGpioStatuses();
+    }
+
+    @Override
+    public GPIOAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.gpio_status, parent, false);
+
+        return new ViewHolder((CardView) view);
+    }
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
+        holder.switcher.setChecked(gpioStatusList.get(position).getStatus());
+        holder.gpioId.setText(Integer.toString(gpioStatusList.get(position).getId()));
+
+        final int teaColor = context.getResources().getColor(R.color.tea_color);
+        holder.gpioId.setTextColor(holder.switcher.isChecked() ? teaColor : Color.RED);
+
+        holder.switcher.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (!NetworkUtil.isNetworkAvailable(context)) {
+                    NetworkUtil.showNetworkDialog(context);
+                    buttonView.setChecked(!isChecked);
+                    return;
+                }
+
+                //Skips system recycler invoking
+                if (!buttonView.isPressed()) return;
+
+                gpioStatusList.get(position).setStatus(isChecked);
+
+                KaaClient kaaClient = KaaProvider.getClient(holder.cardView.getContext());
+
+                EventFamilyFactory eventFamilyFactory = kaaClient.getEventFamilyFactory();
+                final RemoteControlECF ecf = eventFamilyFactory.getRemoteControlECF();
+                holder.gpioId.setTextColor(buttonView.isChecked() ? teaColor : Color.RED);
+                ecf.sendEvent(new GpioToggleRequest(gpioStatusList.get(position)), device.getKaaEndpointId());
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return device.getGpioStatuses().size();
+    }
+}

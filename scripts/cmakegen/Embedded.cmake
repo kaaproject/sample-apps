cbb1af0becbbd6d53befa18931d190ab09a72d5c
RostakaGmfun <rostawesomegd@gmail.com>
2016-10-03 17:29:04 +0300

    APP-329: [C/C++] Distinguish between CMake variables and compile definitions in cmakegen

--- scripts/cmakegen/Embedded.cmake
+++ scripts/cmakegen/Embedded.cmake
@@ -3,8 +3,8 @@
 # The blank.c file is a placeholder for CMake's add_executable()
 # All the code (Kaa SDK, ESP8266 SDK and demo) is compiled as static libraries
 # and linked into that executable.
-add_subdirectory(targets/${KAA_TARGET})
-if("${KAA_TARGET}" STREQUAL "esp8266")
+add_subdirectory(targets/${KAA_PLATFORM})
+if("${KAA_PLATFORM}" STREQUAL "esp8266")
     add_library(demo_client_s STATIC %APP_SOURCES)
     file(WRITE ${CMAKE_BINARY_DIR}/blank.c "")
     add_executable(demo_client ${CMAKE_BINARY_DIR}/blank.c)

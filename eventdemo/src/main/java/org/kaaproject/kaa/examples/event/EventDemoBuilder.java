3436496700d23c2349924ea1622e658a4bb9edea
Anthony Shkurenko <tonyshkurenko@gmail.com>
2016-10-24 13:52:33 +0300

    APP-374: Try to fix EventDemoBuilder.java

--- eventdemo/src/main/java/org/kaaproject/kaa/examples/event/EventDemoBuilder.java
+++ eventdemo/src/main/java/org/kaaproject/kaa/examples/event/EventDemoBuilder.java
@@ -1,17 +1,17 @@
 /**
- *  Copyright 2014-2016 CyberVision, Inc.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Copyright 2014-2016 CyberVision, Inc.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.kaaproject.kaa.examples.event;
@@ -84,12 +84,6 @@ public class EventDemoBuilder extends AbstractDemoBuilder {
         sdkProfileDto.setNotificationSchemaVersion(1);
         sdkProfileDto.setLogSchemaVersion(1);
 
-        projectsSdkMap.put(EVENT_DEMO_JAVA_ID, sdkProfileDto);
-        projectsSdkMap.put(EVENT_DEMO_CPP_ID, sdkProfileDto);
-        projectsSdkMap.put(EVENT_DEMO_C_ID, sdkProfileDto);
-        projectsSdkMap.put(EVENT_DEMO_OBJC_ID, sdkProfileDto);
-        projectsSdkMap.put(EVENT_DEMO_ANDROID_ID, sdkProfileDto);
-
         loginTenantDeveloper(client);
 
         ApplicationEventFamilyMapDto thermoAefMap = mapEventClassFamily(client, eventApplication, thermoEventClassFamily);
@@ -112,6 +106,12 @@ public class EventDemoBuilder extends AbstractDemoBuilder {
         sdkProfileDto.setDefaultVerifierToken(trustfulUserVerifier.getVerifierToken());
 
         logger.info("Finished loading 'Event Demo Application' data.");
+
+        projectsSdkMap.put(EVENT_DEMO_JAVA_ID, sdkProfileDto);
+        projectsSdkMap.put(EVENT_DEMO_CPP_ID, sdkProfileDto);
+        projectsSdkMap.put(EVENT_DEMO_C_ID, sdkProfileDto);
+        projectsSdkMap.put(EVENT_DEMO_OBJC_ID, sdkProfileDto);
+        projectsSdkMap.put(EVENT_DEMO_ANDROID_ID, sdkProfileDto);
     }
 
     @Override

783f6c879978c1e77b58209efc1c0a9f185a19e2
ashevchenko <ashevchenko@cybervisiontech.com>
2016-11-21 12:55:07 +0200

    APP-482: [C] Fix after code review

--- common/c/targets/cc32xx/target.h
+++ common/c/targets/cc32xx/target.h
@@ -33,15 +33,7 @@
 
 #include <stdlib.h>
 
-
-
-
-
-
 #define demo_printf(msg, ...) UART_PRINT((msg), ##__VA_ARGS__)
-
-//#define NUM_GPIO_LEDS 5
-//extern const char *led_name[NUM_GPIO_LEDS];
 
 #define TARGET_DEVICE_NAME "CC3200"
 #define TARGET_MODEL_NAME "LaunchPad"

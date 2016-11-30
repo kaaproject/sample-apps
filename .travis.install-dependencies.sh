b3e4c5d7c38f5daf08f91a3f9b0afe5376be7203
Anatoly Yarinich <anatoly.yarinich@gmail.com>
2016-11-30 17:42:54 +0200

    KSB-24: Added configuration for Travis

---
+++ .travis.install-dependencies.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+MAVEN_VERSION=3.3.9
+
+wget http://apache.cs.utah.edu/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz
+
+tar -zxf apache-maven-${MAVEN_VERSION}-bin.tar.gz
+mv apache-maven-${MAVEN_VERSION} /usr/local
+ln -s /usr/local/apache-maven-${MAVEN_VERSION}/bin/mvn /usr/bin/mvn
+
+echo "export M2_HOME=/usr/local/apache-maven-${MAVEN_VERSION}" >> ~/.profile
+source ~/.profile
+
+echo "Maven is on version `mvn -v`"

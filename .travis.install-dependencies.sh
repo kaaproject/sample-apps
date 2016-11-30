24672b9205769985db3d89f5d15122b1ea406486
Anatoly Yarinich <anatoly.yarinich@gmail.com>
2016-11-30 17:42:54 +0200

    APP-491: Updated configuration for Travis

---
+++ .travis.install-dependencies.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+MAVEN_VERSION=3.3.9
+MAVEN_EXECUTOR="./.travis.maven.sh"
+
+wget http://apache.cs.utah.edu/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz
+
+tar -zxf apache-maven-${MAVEN_VERSION}-bin.tar.gz
+mv apache-maven-${MAVEN_VERSION} /usr/local
+ln -s /usr/local/apache-maven-${MAVEN_VERSION}/bin/mvn /usr/bin/mvn
+
+echo "#!/bin/bash" > ${MAVEN_EXECUTOR}
+echo "set -ev" >> ${MAVEN_EXECUTOR}
+echo "export M2_HOME=/usr/local/apache-maven-${MAVEN_VERSION}" >> ${MAVEN_EXECUTOR}
+echo "export MAVEN_BASEDIR=/usr/local/apache-maven-${MAVEN_VERSION}" >> ${MAVEN_EXECUTOR}
+echo "/usr/bin/mvn" "\"\$@\"" >> ${MAVEN_EXECUTOR}
+
+chmod +x ${MAVEN_EXECUTOR}
+
+echo "Dumping script content:"
+echo "---------------------------------------"
+cat ${MAVEN_EXECUTOR}
+echo "---------------------------------------"
+
+echo "Checking Maven version..."
+${MAVEN_EXECUTOR} -v

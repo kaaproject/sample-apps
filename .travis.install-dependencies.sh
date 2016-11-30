e1d50bedcae9ae21e47aff5424cdff7cdad42d44
Anatoly Yarinich <anatoly.yarinich@gmail.com>
2016-11-30 19:35:58 +0200

    KSB-24: Updated maven installation script for Travis

--- .travis.install-dependencies.sh
+++ .travis.install-dependencies.sh
@@ -2,6 +2,7 @@
 set -ev
 
 MAVEN_VERSION=3.3.9
+MAVEN_EXECUTOR="./.travis.maven.sh"
 
 wget http://apache.cs.utah.edu/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz
 
@@ -9,7 +10,10 @@ tar -zxf apache-maven-${MAVEN_VERSION}-bin.tar.gz
 mv apache-maven-${MAVEN_VERSION} /usr/local
 ln -s /usr/local/apache-maven-${MAVEN_VERSION}/bin/mvn /usr/bin/mvn
 
-echo "export M2_HOME=/usr/local/apache-maven-${MAVEN_VERSION}" >> ~/.profile
-source ~/.profile
+echo "#!/bin/bash" > ${MAVEN_EXECUTOR}
+echo "export M2_HOME=/usr/local/apache-maven-${MAVEN_VERSION}" >> ${MAVEN_EXECUTOR}
+echo "mvn" "$@" >> ${MAVEN_EXECUTOR}
 
-echo "Maven is on version `mvn -v`"
+chmod +x ${MAVEN_EXECUTOR}
+
+echo "Maven is on version `${MAVEN_EXECUTOR} -v`"

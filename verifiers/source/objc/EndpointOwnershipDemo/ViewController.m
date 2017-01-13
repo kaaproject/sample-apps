ab20bd3eb2ade45cffc7710833990e0903e4b650
Aleksey Gutyro <agutyro@cybervisiontech.com>
2017-01-13 16:54:11 +0200

    Authorization via twitter fixed and returned back.

--- verifiers/source/objc/EndpointOwnershipDemo/ViewController.m
+++ verifiers/source/objc/EndpointOwnershipDemo/ViewController.m
@@ -54,19 +54,19 @@ typedef NS_ENUM(int, AuthorizationLabel) {
     [GIDSignIn sharedInstance].uiDelegate = self;
     [GIDSignIn sharedInstance].delegate = self;
     
-    self.twtrLogInButton.hidden = YES;
-//    self.twtrLogInButton.loginMethods = TWTRLoginMethodAll;
-//    self.twtrLogInButton.logInCompletion = ^(TWTRSession *session, NSError *error) {
-//        if (session) {
-//            NSLog(@"Signed in as %@", [session userName]);
-//            self.user = [[User alloc] initWithUserId:session.userID
-//                                               token:session.authToken
-//                                   authorizedNetwork:AuthorizedNetworkTwitter];
-//            [self updateSocialNetworkAuthorizationWithStatus:YES];
-//        } else {
-//            NSLog(@"error: %@", [error localizedDescription]);
-//        }
-//    };
+    self.twtrLogInButton.loginMethods = TWTRLoginMethodAll;
+    self.twtrLogInButton.logInCompletion = ^(TWTRSession *session, NSError *error) {
+        if (session) {
+            NSLog(@"Signed in as %@", [session userName]);
+            NSString *tokenAndSecretString = [NSString stringWithFormat:@"%@ %@", session.authToken, session.authTokenSecret];
+            self.user = [[User alloc] initWithUserId:session.userID
+                                               token:tokenAndSecretString
+                                   authorizedNetwork:AuthorizedNetworkTwitter];
+            [self updateSocialNetworkAuthorizationWithStatus:YES];
+        } else {
+            NSLog(@"error: %@", [error localizedDescription]);
+        }
+    };
     
     self.kaaManager = [KaaManager sharedInstance];
     [self.kaaManager startKaaClient];
@@ -77,14 +77,14 @@ typedef NS_ENUM(int, AuthorizationLabel) {
     
     switch (self.user.network) {
         case AuthorizedNetworkFacebook:
-//            self.twtrLogInButton.hidden = status;
+            self.twtrLogInButton.hidden = status;
             self.googleLogInButton.hidden = status;
             break;
             
         case AuthorizedNetworkTwitter:
         case AuthorizedNetworkGoogle:
             self.fbLoginButton.hidden = status;
-//            self.twtrLogInButton.hidden = status;
+            self.twtrLogInButton.hidden = status;
             self.googleLogInButton.hidden = status;
             self.logOutButton.hidden = !status;
             break;

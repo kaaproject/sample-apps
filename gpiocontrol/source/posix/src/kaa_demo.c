f5f615898d02cab04f8b9457256b66d19d9a8488
Sergii PAradiuk <sparadiuk@cybervisiontech.com>
2017-02-17 19:56:19 +0200

    Fixed bugs

--- gpiocontrol/source/posix/src/kaa_demo.c
+++ gpiocontrol/source/posix/src/kaa_demo.c
@@ -26,17 +26,22 @@
 #include <kaa/gen/kaa_remote_control_ecf.h>
 #include <extensions/user/kaa_user.h>
     
-#define KAA_USER_ID                "kaademo"
-#define KAA_USER_ACCESS_TOKEN      "kaademo"
-#define GPIO_COUNT                 3
+#define KAA_USER_ID                       "kaademo"
+#define KAA_USER_ACCESS_TOKEN             "kaademo"
+#define KAA_MAX_NUMBER_BYTES_COMMAND      20
+#define KAA_MAX_NUMBER_ACCESS_TOKEN       40
+
+// All commands which support.
+#define KAA_COMMAND_TOGGLE "toggle"
+#define KAA_COMMAND_STATUS "status"
 
 #define demo_printf                printf
 #define demo_scanf                 scanf
 
-// Static variables
+// Static variables.
 static kaa_client_t *kaa_client = NULL;
 
-// Static functions 
+// Static functions.
 static void kaa_device_info_response(
     void *context, 
     kaa_remote_control_ecf_device_info_response_t *event, 
@@ -44,9 +49,18 @@ static void kaa_device_info_response(
 {
     (void)context;
     (void)source;
+    uint32_t size;
     kaa_remote_control_ecf_gpio_status_t* gpio_status;
-    gpio_status  = kaa_list_get_data(kaa_list_begin(event->gpio_status));
-    demo_printf("Status : %d, ID : %d\n", gpio_status->status, gpio_status->id);
+    kaa_list_node_t * node;
+    
+    size = kaa_list_get_size(event->gpio_status);    
+    node = kaa_list_begin(event->gpio_status);
+
+    for (uint32_t i = 0; i < size; i++) {
+        gpio_status  = kaa_list_get_data(node);
+        node = kaa_list_next(node);
+        demo_printf("Status : %d, ID : %d\r\n", gpio_status->status, gpio_status->id);
+    }
 }
   
 static void kaa_send_kaa_remote_control_ecf_gpio_toggle_request(void *context)
@@ -55,20 +69,17 @@ static void kaa_send_kaa_remote_control_ecf_gpio_toggle_request(void *context)
     int id;
     int state;
     int size;
+    kaa_remote_control_ecf_gpio_toggle_request_t *event;
     static const char *type = "test";
     
     size = demo_scanf("%d %d", &id, &state);
 
     if (size <= 0) {
-        demo_printf("Erorr set values\n");
+        demo_printf("Failed set values\r\n");
         return;
     }
-    if ((id < 0) || (id >= GPIO_COUNT)) {
-        demo_printf("Erorr pin ID:  %d\n", id);
-        return;
-    }  
 
-    kaa_remote_control_ecf_gpio_toggle_request_t *event = kaa_remote_control_ecf_gpio_toggle_request_create();
+    event = kaa_remote_control_ecf_gpio_toggle_request_create();
     assert(event);
     event->gpio = kaa_remote_control_ecf_gpio_status_create();
     assert(event->gpio);
@@ -95,37 +106,37 @@ static void kaa_send_kaa_remote_control_ecf_device_info_request(void *context)
 
 static void kaa_user_callback(void *context)
 {
-    char command[21];
+    char command[KAA_MAX_NUMBER_BYTES_COMMAND];
     int size;
     demo_printf(">");
-    size = demo_scanf("%20s", command);
+    size = demo_scanf("%19s", command);
     
     if (size <= 0) {
-        demo_printf("IErorr set values\n");
+        demo_printf("Failed set values\r\n");
         return;
     }
-
-    if (!strcmp(command, "toggle")) {
+    // Check a command.
+    if (!strncmp(command, KAA_COMMAND_TOGGLE, strlen(KAA_COMMAND_TOGGLE))) {
         kaa_send_kaa_remote_control_ecf_gpio_toggle_request(context);
-    } else if (!strcmp(command, "status")) {
+    } else if (!strncmp(command, KAA_COMMAND_STATUS, strlen(KAA_COMMAND_STATUS))) {
         kaa_send_kaa_remote_control_ecf_device_info_request(context);
     } else {
-        demo_printf("Fail command ..\n");
+        demo_printf("An incorrect command ..\r\n");
     }
 }
 
 int main(void)
 {
     int size;
-    char endpoint_access_token[40];
-    kaa_error_t error_code = -1;
+    char endpoint_access_token[KAA_MAX_NUMBER_ACCESS_TOKEN];
+    kaa_error_t error_code = KAA_ERR_GENERIC;
 
     demo_printf("GPIO demo started\r\n");
-    demo_printf("Set endpoint access token : ");
+    demo_printf("Set endpoint access token: ");
     size = demo_scanf("%39s", endpoint_access_token);   
 
     if (size <= 0) {
-        demo_printf("Erorr set value\n");
+        demo_printf("Failed set value: %i\r\n", KAA_ERR_GENERIC);
         return error_code;
     }
 	
@@ -141,15 +152,10 @@ int main(void)
         demo_printf("Failed attach to user: %i\r\n", error_code);
         return error_code;
     }
-    
-    if (error_code) {
-        demo_printf("Failed attach to user: %i\r\n", error_code);
-        return error_code;
-    }
-    
+
     error_code = kaa_user_manager_attach_endpoint(kaa_client_get_context(kaa_client)->user_manager, endpoint_access_token, NULL);
     if (error_code) {
-        demo_printf("Failed attach to endpoint(: %i\r\n", error_code);
+        demo_printf("Failed attach to endpoint: %i\r\n", error_code);
         return error_code;
     }
     
